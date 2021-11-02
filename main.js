5135dbdffcb0a0f3ec9927a013271b3ecd4c9c41
Tasin Ishmam <tasinishmam@gmail.com>
2021-11-02 20:48:16 +0600

    Copy sqlite db to userData folder

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
 const { PrismaClient } = require('@prisma/client');
 const { app, BrowserWindow } = require('electron')
+const { join } = require('path');
+const fs = require('fs')
 const path = require('path')
-let prisma = new PrismaClient();
-
+const isDev = require('electron-is-dev');
 const { ipcMain, dialog } = require('electron');
 const { electron } = require('process');
 
 
+const dbPath = isDev ? join(__dirname, './prisma/dev.db') : path.join(app.getPath("userData"), "database.db")
 
-ipcMain.on('prisma-create-user', async (event, arg) => {
-    
-    let count = await prisma.user.count({});
-    await prisma.user.create({
-        data: {
-            email: `email-${count+1}`,
-            name: `name-${count+1}`
+if (!isDev) {
+    try {
+        // database file does not exist, need to create
+        fs.copyFileSync(join(process.resourcesPath, 'prisma/dev.db'), dbPath, fs.constants.COPYFILE_EXCL)
+        console.log("New database file created")
+    } catch (err) {
+        if (err.code != "EEXIST") {
+            console.error(`Failed creating sqlite file.`, err)
+        } else {
+            console.log("Database file detected")
         }
-    });
-    
-})
+    }
+}
 
-ipcMain.on('prisma-button-request', async (event) => {
-    console.log("Main Thread: Requesting all user data through IPC.")
-    event.sender.send('prisma-button-response', JSON.stringify(await prisma.user.findMany({})))
-})
+
+let prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: `file:${dbPath}`,
+        },
+    },
+});
+
+
+console.log(`Is Production?: ${!isDev}`)
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -32,7 +43,6 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
-            // preload: path.join(__dirname, 'preload.js')
         }
     })
 
@@ -40,10 +50,7 @@ function createWindow() {
 }
 
 app.whenReady().then(async () => {
-    console.log(await prisma.user.findMany({}))
     createWindow()
-
-
 
 })
 
@@ -54,4 +61,21 @@ app.on('activate', function () {
 app.on('window-all-closed', function () {
     console.log("Closing window")
     if (process.platform !== 'darwin') app.quit()
+})
+
+ipcMain.on('prisma-create-user', async (event, arg) => {
+    console.log("Creating new user")
+    let count = await prisma.user.count({});
+    await prisma.user.create({
+        data: {
+            email: `email-${count + 1}`,
+            name: `name-${count + 1}`
+        }
+    });
+
+})
+
+ipcMain.on('prisma-button-request', async (event) => {
+    console.log("Main Thread: Requesting all user data through IPC.")
+    event.sender.send('prisma-button-response', JSON.stringify(await prisma.user.findMany({})))
 })
